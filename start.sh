63c64a34d31567e0fb12c5384b21f45080c2a661
abhithemachinegun <117830155+abhithemachinegun@users.noreply.github.com>
2023-05-26 16:46:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/abhithemachinegun/AutoFilterBot.git /AutoFilterBot
+  git clone https://github.com/abhithemachinegun/AutoFilterBotaditya.git /AutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterBot
